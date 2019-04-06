e59ace7e236f97f369528cf82781bace63da9e79
alexcmx <alexey.work3@yandex.ru>
2019-04-06 15:39:06 +0300

    client

--- Project/client/models.py
+++ Project/client/models.py
@@ -6,6 +6,12 @@ class Client(Adress):
     login = models.CharField(max_length=255)
     passwd = models.CharField(max_length=255)
 
+class RequestClient(Adress):
+    progress = models.IntegerField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    dron = models.ForeignKey(Drone, on_delete=models.CASCADE)
+
 
 
 
