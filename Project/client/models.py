04ae6ba3ab28839937ef3250363df7817fe283db
alexcmx <alexey.work3@yandex.ru>
2019-04-06 15:29:50 +0300

    +1

--- Project/client/models.py
+++ Project/client/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from Project.stock.models import *
+
+class Client():
+    name = models.CharField(max_length=255)
+    login = models.CharField(max_length=255)
+    passwd = models.CharField(max_length=255)
+    addr = Adress()
+
+
+
+
 
 
 # Create your models here.
