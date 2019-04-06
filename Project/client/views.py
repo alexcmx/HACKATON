8ae03c20dd65b57e01846d64881970c34a165e37
alexcmx <alexey.work3@yandex.ru>
2019-04-06 16:54:26 +0300

    serializers view

--- Project/client/views.py
+++ Project/client/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from client.models import RequestClient, Client
+from client.serializers import RequestClientSerializer, ClientSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class RequestClientViewSet(viewsets.ModelViewSet):
+    queryset = RequestClient.objects.all()
+    serializer_class = RequestClientSerializer
+
