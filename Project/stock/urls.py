33ceb61363b8a6d1ab3fe5276d1bd99861912546
hvidsmen <hvidsmen@mail.ru>
2019-04-06 17:51:18 +0300

    Filter

--- Project/stock/urls.py
+++ Project/stock/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 from stock.views import StockViewSet, PromiseViewSet, TimeWorkViewSet, DroneViewSet
 from Project.urls import router
+from stock.filters import StockList, PromiseList, DroneList, TimeWorkList
+
+urlpatterns = [
+    path('stocks', StockList.as_view()),
+    path('promises', PromiseList.as_view()),
+    path('drones', DroneList.as_view()),
+    path('time-works', TimeWorkList.as_view()),
+]
