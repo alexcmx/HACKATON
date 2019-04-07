7bc02b86580b5023c8e120ec6bde962483166d8c
hvidsmen <hvidsmen@mail.ru>
2019-04-07 04:20:44 +0300

    Добавлены html старницы для заполнения

--- Project/base/views.py
+++ Project/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
     template_name = "base/index.html"
 # Create your views here.
+
 
 class DronOrderView(TemplateView):
     template_name = "base/dron-order.html"
@@ -11,3 +13,11 @@ class DronOrderView(TemplateView):
 
 class DronFindMeView(TemplateView):
     template_name = "base/dron-find-me.html"
+
+
+class RequestView(TemplateView):
+    template_name = 'base/request.html'
+
+
+class TruckView(TemplateView):
+    template_name = 'base/truck.html'
