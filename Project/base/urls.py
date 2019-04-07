56836eeabcf8a1273d476aa9834197eb032d747d
alexcmx <alexey.work3@yandex.ru>
2019-04-07 13:13:19 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit fc101e5ebe970ec13503099e4624660af46075a5, reversing
    changes made to f93a27f6c74fdec080f395eecc858881e4ce97a0.

--- Project/base/urls.py
+++ Project/base/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from base.views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('dron-find-me/', DronFindMeView.as_view(), name='dron-find-me'),
+    path('',IndexView.as_view(), name='index'),
+    path('dron-find-me/',DronFindMeView.as_view(), name='dron-find-me'),
     path('dron-order/', DronOrderView.as_view(), name="dron-order"),
-    path('request/', RequestView.as_view(), name="request"),
-    path('truck/', TruckView.as_view(), name="truck"),
-
+    path('map/', MapView.as_view(), name='map'),
 
 ]
--- Project/base/views.py
+++ Project/base/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
 class IndexView(TemplateView):
     template_name = "base/index.html"
 # Create your views here.
-
 
 class DronOrderView(TemplateView):
     template_name = "base/dron-order.html"
@@ -15,9 +13,5 @@ class DronFindMeView(TemplateView):
     template_name = "base/dron-find-me.html"
 
 
-class RequestView(TemplateView):
-    template_name = 'base/request.html'
-
-
-class TruckView(TemplateView):
-    template_name = 'base/truck.html'
+class MapView(TemplateView):
+    template_name = "base/map.html"
